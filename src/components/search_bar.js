36958118e860c209acea5202453dd63c5561d693
taniyow <markcmtan@gmail.com>
2023-01-09 00:55:27 +0800

    React Components Structure, Functional & Class Based Components

---
+++ src/components/search_bar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    render() {
+        return <input onChange={(e) => console.log(e.target.value)} type="text" />
+    }
+
+    onInputChange(e) {
+        console.log(e.target.value);
+    }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyCQNrhNGcH7fJaSPAre9QL2H-fE67xwjA8';
 
+// Create a new component. This component should produce some HTML
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM).
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <App/>
+, document.querySelector('.container'))

2b849912d902e0ec1c914eea6071d6556499638f
taniyow <markcmtan@gmail.com>
2023-01-24 00:34:34 +0800

    Adding YTSearch Response

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCQNrhNGcH7fJaSPAre9QL2H-fE67xwjA8';
 
 // Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos })
+        });
+        // this works if the function(videos) and the key value
+        // on setState is the same ({ videos: videos })
+        
+        // YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+        //     this.setState({ videos: data });
+        // })
+    }
+
+    render () {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM).
 ReactDOM.render(
     <App/>
-, document.querySelector('.container'))
+, document.querySelector('.container'));

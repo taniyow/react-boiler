e2aa966dd4d2d492f32a08bbbbe963320f9e2979
taniyow <markcmtan@gmail.com>
2023-01-24 00:52:10 +0800

    Passing Props

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCQNrhNGcH7fJaSPAre9QL2H-fE67xwjA8';
 
@@ -28,6 +29,8 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos} /> 
+                {/* videos={this.state.videos} is a passing props */}
             </div>
         );
     }
